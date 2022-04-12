1b052484bace9215485cd7b05440037a220f3d8d
Barbara Rutkowska <89747794+Basixx@users.noreply.github.com>
2022-04-13 01:29:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
    const apiRoot = 'https://kodilla-tasks-bsx.herokuapp.com/v1/tasks';
-   const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+   const trelloApiRoot = 'https://kodilla-tasks-bsx.herokuapp.com/v1/tasks';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
 
